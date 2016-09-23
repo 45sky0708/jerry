51b1461354bc2cb813d27c7634346a1728b4aff5
EK3286 <EK3286@SK11J600-88>
2016-09-23 16:54:57 +0800

    加入專案檔案。

---
+++ Emp0910/Controllers/FormDemoController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Emp0910.Models;
+using System.Data.Entity;
+
+namespace Emp0910.Controllers
+{
+    public class FormDemoController : Controller
+    {
+        /// <summary>
+        /// 首頁
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// partail view
+        /// </summary>
+        /// <returns></returns>
+        public PartialViewResult ShowData(string empnumber ="" ) 
+        {
+            EmpEntity objEmpList = new EmpEntity();
+            FormViewModel formvm = new FormViewModel();
+            
+            //表單檢核
+            if (!string.IsNullOrWhiteSpace(empnumber))
+            {
+                //撈輸入empnumber的那筆資料
+                int empid = int.Parse(empnumber);
+                formvm.empdata = (from P in objEmpList.EmpPersonal
+                               join U in objEmpList.EmpUnit on P.EmpNumber equals U.EmpNumber
+                               where P.EmpNumber == empid
+                                  select new EmpViewModel
+                               {
+                                   EmpNumber = P.EmpNumber,
+                                   EmpName = P.EmpName,
+                                   EmpSex = P.EmpSex,
+                                   EmpBirthday = P.EmpBirthday,
+                                   CreationDate = P.CreationDate,
+                                   LastloginDate = P.LastloginDate,
+                                   Seniority = P.Seniority,
+                                   UnitNumber = U.UnitNumber,
+                                   UnitName = U.UnitName,
+                                   JobTitle = U.JobTitle
+                               }).ToList();
+                
+                if (formvm.empdata.Count == 0)
+                {
+                    formvm.IsSuccess = false;
+                    formvm.Msg = "No Data.";
+                }
+                else
+                {
+                    formvm.IsSuccess = true;
+                    formvm.Msg = "搜尋成功";
+                }
+            }
+            else
+            {
+                formvm.IsSuccess = true; //第一次，未有動作，顯示未搜尋
+                formvm.Msg = "未搜尋";                                
+                formvm.empdata = (from P in objEmpList.EmpPersonal
+                                  join U in objEmpList.EmpUnit on P.EmpNumber equals U.EmpNumber                                 
+                                  select new EmpViewModel
+                                  {
+                                      EmpNumber = P.EmpNumber,
+                                      EmpName = P.EmpName,
+                                      EmpSex = P.EmpSex,
+                                      EmpBirthday = P.EmpBirthday,
+                                      CreationDate = P.CreationDate,
+                                      LastloginDate = P.LastloginDate,
+                                      Seniority = P.Seniority,
+                                      UnitNumber = U.UnitNumber,
+                                      UnitName = U.UnitName,
+                                      JobTitle = U.JobTitle
+                                  }).ToList();
+            }
+            
+
+            return PartialView(formvm);
+        }
+
+        /// <summary>
+        /// 新增
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Creat() 
+        {
+            EmpEntity emp = new EmpEntity();
+            EmpViewModel empcreat = new EmpViewModel();
+            empcreat.EmpNumber = emp.EmpPersonal.Select(x => x.EmpNumber).Max(); //找出編號最大，並加1
+            empcreat.EmpNumber++;
+            return View(empcreat);
+        }
+
+        [HttpPost]
+        public JsonResult Creat(EmpPersonal Pdatacreat,EmpUnit Udatacreat)
+        {
+            using (EmpEntity db = new EmpEntity())
+                {
+                    db.EmpPersonal.Add(Pdatacreat);
+                    db.EmpUnit.Add(Udatacreat);
+                    db.SaveChanges();
+                }            
+            return Json(JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// 修改
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Edit (int empnumber) 
+        {
+
+            EmpEntity objEmpList = new EmpEntity();
+            var emplist = (from P in objEmpList.EmpPersonal
+                           join U in objEmpList.EmpUnit on P.EmpNumber equals U.EmpNumber
+                           where P.EmpNumber == empnumber 
+                           select new EmpViewModel
+                           {
+                               EmpNumber = P.EmpNumber,
+                               EmpName = P.EmpName,
+                               EmpSex = P.EmpSex,
+                               EmpBirthday = P.EmpBirthday,
+                               CreationDate = P.CreationDate,
+                               LastloginDate = P.LastloginDate,
+                               Seniority = P.Seniority,
+                               UnitNumber = U.UnitNumber,
+                               UnitName = U.UnitName,
+                               JobTitle = U.JobTitle
+                           }).FirstOrDefault();
+
+
+
+            return View(emplist);
+        }
+
+        [HttpPost]
+        public JsonResult Edit(EmpViewModel datasava)
+        {           
+            using (EmpEntity db = new EmpEntity())
+            {
+                EmpPersonal P = db.EmpPersonal.Where(x => x.EmpNumber == datasava.EmpNumber).FirstOrDefault();
+                P.EmpName = datasava.EmpName;
+                P.EmpSex = datasava.EmpSex;
+                P.EmpBirthday = datasava.EmpBirthday;
+                P.CreationDate = datasava.CreationDate;
+                P.LastloginDate = datasava.LastloginDate;
+                P.Seniority = datasava.Seniority;
+
+                EmpUnit U = db.EmpUnit.Where(x => x.EmpNumber == datasava.EmpNumber).FirstOrDefault();
+                U.UnitName = datasava.UnitName;
+                U.UnitNumber = datasava.UnitNumber;
+                U.JobTitle = datasava.JobTitle;
+
+
+                db.SaveChanges();
+            }
+
+            return Json(JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
---
+++ Emp0910/Models/ViewModel/EmpViewModel.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace Emp0910.Models
+{
+    //[Serializable]
+
+    //資料庫資料
+    public class EmpViewModel
+    {
+        public int EmpNumber {get; set;}
+        [Required(ErrorMessage = "EmpSex is required")]
+        public string EmpName { get; set; }
+        [Required(ErrorMessage = "EmpSex is required")]
+        public string EmpSex { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy/MM/dd}")]
+        public Nullable<System.DateTime> EmpBirthday { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy/MM/dd}")]
+        public Nullable<System.DateTime> CreationDate { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy/MM/dd}")]
+        public Nullable<System.DateTime> LastloginDate { get; set; }
+        public string Seniority { get; set; }
+        public string UnitNumber { get; set; }
+        public string UnitName { get; set; }
+        public string JobTitle { get; set; }
+        //datatime to string
+        public string EmpBirthdayDataFormat //EmpBirthday to String("yyyy/MM/dd") 
+        {
+            get
+            {
+                string formatafter = Convert.ToDateTime(this.EmpBirthday).ToString("yyyy/MM/dd");
+                return formatafter;
+            }
+        }
+
+        public string CreationDateDataFormat
+        {
+            get
+            {
+                string formatafter = Convert.ToDateTime(this.CreationDate).ToString("yyyy/MM/dd");
+                return formatafter;
+            }
+        }
+        public string LastloginDateDataFormat
+        {
+            get
+            {
+                string formatafter = Convert.ToDateTime(this.LastloginDate).ToString("yyyy/MM/dd");
+                return formatafter;
+            }
+        }
+
+    }
+
+    //表單檢核
+    public class FormViewModel
+    {
+        public List<EmpViewModel> empdata;
+        public bool IsSuccess { get; set; }
+        public string Msg { get; set; }
+
+        public FormViewModel()
+        {
+            empdata = new List<EmpViewModel>();
+        }       
+    }
+}
+
+//public string EmpBirthdayDataFormat //EmpBirthday to String("yyyy/MM/dd") 
+//{
+//    get
+//    {
+//        //DateTime.TryParse()
+//        string formatafter = string.Empty;
+//        if (this.EmpBirthday == null)       //不能有NULL
+//            formatafter = "";
+//        else
+//            formatafter = Convert.ToDateTime(this.EmpBirthday).ToString("yyyy/MM/dd");
+//        return formatafter;
+//    }
+//}
